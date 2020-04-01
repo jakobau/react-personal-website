e19edc03f8e67602c66bd4f9e8d5f6cc903ff4d5
jakobau <30506552+jakobau@users.noreply.github.com>
2020-03-31 14:16:44 -1000

    adding big things

--- react-gh-pages/src/components/section.js
+++ react-gh-pages/src/components/section.js
@@ -1,16 +1,66 @@
 import React, { Component } from 'react'
 
 function Title(props) {
-  return <h1>$&#10100;{props.name}&#10101;</h1>;
+  return (
+    <h1>
+      <span id="smaller">$&#10100;</span>
+      <a href={props.link}> {props.name} </a>
+      <span id="smaller">&#10101;</span>
+    </h1>
+  );
 }
 
-export default class Section extends React.Component {
+function Date(props) {
+  return <p>Sept 2018 - Sept 2019</p>
+}
+
+function Desc(props) {
+  return <h3><em>{props.desc}</em></h3>
+}
+
+function Label(props) {
+  return <span className="label">{props.label}</span>
+}
+
+export default class Section extends Component {
   render() {
     return (
-      <div id="section">
-        <Title name="Twitterphile" />
-        <Title name="Fantazmix" />
-        <Title name="discord-bot" />
+      <div id="parent">
+        <div id="section">
+          <Title name="Twitterphile" link="#" />
+          <Desc desc="use of the Twitter API" />
+          <div id="labels">
+            <Label label="Twitter API" />
+            <Label label="Node" />
+            <Label label="Express" />
+            <Label label="JavaScript" />
+            <Label label="HTML/CSS" />
+            <Label label="Bootstrap 4" />
+          </div>
+        </div>
+        <div id="section">
+          <Title name="Fantazmix" link="#" />
+          <Desc desc="music generating web app" />
+          <div id="labels">
+            <Label label="Django" />
+            <Label label="Git" />
+            <Label label="Python" />
+            <Label label="Google Magenta" />
+            <Label label="HTML/CSS" />
+          </div>
+        </div>
+        <div id="section">
+          <Title name="discord-bot" link="#" />
+          <Desc desc="general discord bot" />
+          <div id="labels">
+            <Label label="Twitter API" />
+            <Label label="Node" />
+            <Label label="Express" />
+            <Label label="JavaScript" />
+            <Label label="HTML/CSS" />
+            <Label label="Bootstrap 4" />
+          </div>
+        </div>
       </div>
     );
   }
