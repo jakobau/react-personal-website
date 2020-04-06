d95ea72f15728095c9a2e1dd493718b0255f9852
jakobau <30506552+jakobau@users.noreply.github.com>
2020-04-05 19:12:49 -1000

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
+
 import Section from './components/section';
 import Profile from './components/profile';
 import Skills from './components/skills';
 import Experience from './components/experience';
 import Footer from './components/footer';
 
+ReactGA.initialize('UA-162783096-1');
+
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, [])
+
   return (
     <div className="App" style={{backgroundColor: '#f7e7cd'}}>
       <span id="top"></span>
@@ -20,5 +30,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
