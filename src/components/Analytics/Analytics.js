84ccc79fc092337eeac59874896440a18eeb021a
Jakob Au <jakeau00@gmail.com>
2022-04-07 06:21:28 -0400

    Update Analytics.js

--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -1,21 +1,16 @@
 // https://github.com/forksociety/AhaEvent/blob/master/src/Components/Analytics/Analytics.js
 
-import React from 'react';
+//import React from 'react';
 import ReactGA from 'react-ga';
-//import * as Sentry from '@sentry/browser';
-
-//import config from 'Config';
+import config from '../../Config/config.json'
 
 const Analytics = () => {
   console.log('$', process.env.NODE_ENV, process.env.NODE_ENV === 'production')
   if (process.env.NODE_ENV === 'production') {
-    /*Sentry.init({
-      dsn: process.env.REACT_APP_SENTRY,
-    });*/
     if(window) {
       const {location: {pathname, search}} = window;
       console.log(pathname + search);
-      ReactGA.initialize('UA-162783096-1');
+      ReactGA.initialize(config.googleAnalyticsID);
       ReactGA.pageview(pathname + search);
     }
   }
